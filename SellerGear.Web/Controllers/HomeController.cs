f78d5bf9f9806c536f21a671cb57e8cd92e03301
Maryan <maryan.kacharaba@gmail.com>
2022-11-19 14:38:10 +0200

    init

---
+++ SellerGear.Web/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SellerGear.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Privacy()
+        {
+            return View();
+        }
+
+        public ActionResult Terms()
+        {
+            return View();
+        }
+
+        public ActionResult Team()
+        {
+            return View();
+        }
+    }
+}
---
+++ SellerGear.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SellerGear.Web.MvcApplication" Language="C#" %>
